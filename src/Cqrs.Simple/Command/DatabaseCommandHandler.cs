7e10c1b213666950abcb482d42f5e0c7fc738318
Sunil Buddala <sunil@donatekart.com>
2020-11-28 23:08:19 +1300

    use database command handler with output

--- src/Cqrs.Simple/Command/DatabaseCommandHandler.cs
+++ src/Cqrs.Simple/Command/DatabaseCommandHandler.cs
@@ -16,4 +16,19 @@ namespace Cqrs.Simple
 
         public abstract Task Handle(TArguments message);
     }
+
+    public abstract class DatabaseCommandHandler<TArguments, TResult> : IHandleCommand<TArguments, TResult>
+        where TArguments : ICommand
+    {
+        protected readonly string Script;
+        protected readonly ISession Session;
+
+        protected DatabaseCommandHandler(ISession session)
+        {
+            Session = session;
+            Script = GetType().GetScript();
+        }
+
+        public abstract Task<TResult> Handle(TArguments message);
+    }
 }
